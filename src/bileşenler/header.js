93a10132dd34705fed4d30a0aa95a45f9a6a6c7f
Kadiraydn01 <kadiraydn01@gmail.com>
2023-07-29 00:10:11 +0300

    Tablar yapıldı

--- src/bileşenler/header.js
+++ src/bileşenler/header.js
@@ -1,29 +1,50 @@
+// GÖREV 1
+// ---------------------
+// Bu fonksiyon argüman olarak `baslik`, `tarih` ve `temp` alarak aşağıdaki yapıyı döndürecek.
+// Kullanılan html etiketleri, öğelerin hiyerarşisi ve öznitelikleri sağlanan işaretlemeyle tam olarak eşleşmelidir!
+// Öğelerin içindeki metin, "textContent" özelliği kullanılarak ayarlanacaktır ("innerText" DEĞİL).
+//
+//  <div class="header">
+//    <span class="date">{ tarih }</span>
+//    <h1>{ baslik }</h1>
+//    <span class="temp">{ yazi }</span>
+//  </div>
+//
+
 const Header = (baslik, tarih, yazi) => {
-  // GÖREV 1
-  // ---------------------
-  // Bu fonksiyon argüman olarak `baslik`, `tarih` ve `temp` alarak aşağıdaki yapıyı döndürecek.
-  // Kullanılan html etiketleri, öğelerin hiyerarşisi ve öznitelikleri sağlanan işaretlemeyle tam olarak eşleşmelidir!
-  // Öğelerin içindeki metin, "textContent" özelliği kullanılarak ayarlanacaktır ("innerText" DEĞİL).
-  //
-  //  <div class="header">
-  //    <span class="date">{ tarih }</span>
-  //    <h1>{ baslik }</h1>
-  //    <span class="temp">{ yazi }</span>
-  //  </div>
-  //
-}
+  const div1 = document.createElement("div");
+  div1.classList.add("header");
+  const span1 = document.createElement("span");
+  span1.classList.add("date");
+  span1.textContent = tarih;
+  const baslik1 = document.createElement("h1");
+  baslik1.textContent = baslik;
+  const span2 = document.createElement("span");
+  span2.classList.add("temp");
+  span2.textContent = yazi;
 
+  div1.appendChild(span1);
+  div1.appendChild(baslik1);
+  div1.appendChild(span2);
+
+  return div1;
+};
+// GÖREV 2
+// ---------------------
+// Tek argümanı olarak bir css seçici alan bu fonksiyonu uygulayın.
+// Görev 1'de tanımladığınız Header bileşenini kullanarak bir header oluşturun (baslik,tarih,yazi parametrelerini kendi isteğinize göre belirleyin)
+// Oluşturulan header'i, verilen seçiciyle eşleşen DOM'daki öğeye eklemelidir.
+//
+// İPUCU: querySelector bir string alabilir (bknz: querySelector("#wrapper"))
+// fakat aynı zamanda bir değişken de alabilir (bknz: querySelector(secici))
 const headerEkleyici = (secici) => {
-  // GÖREV 2
-  // ---------------------
-  // Tek argümanı olarak bir css seçici alan bu fonksiyonu uygulayın.
-  // Görev 1'de tanımladığınız Header bileşenini kullanarak bir header oluşturun (baslik,tarih,yazi parametrelerini kendi isteğinize göre belirleyin)
-  // Oluşturulan header'i, verilen seçiciyle eşleşen DOM'daki öğeye eklemelidir.
-  //
+  const baslik = "Hoşgeldiniz";
+  const tarih = "15 Şubat 1996";
+  const yazi = "Bugün çok güzel bir gün.";
 
-  // İPUCU: querySelector bir string alabilir (bknz: querySelector("#wrapper")) 
-  // fakat aynı zamanda bir değişken de alabilir (bknz: querySelector(secici))
- 
-}
+  const header1 = Header(baslik, tarih, yazi);
+  const sonuc = document.querySelector(secici);
+  sonuc.appendChild(header1);
+};
 
-export { Header, headerEkleyici }
+export { Header, headerEkleyici };
--- src/bileşenler/tablar.js
+++ src/bileşenler/tablar.js
@@ -1,28 +1,52 @@
+// GÖREV 3
+// ---------------------
+// Tek argümanı bir dizi ("konu") olan bu fonksiyonu uygulayın.
+// Örnek olarak, konu dizisi şu şekilde deklare edilmişse ['javascript', 'bootstrap', 'teknoloji']
+// fonksiyon aşağıdaki şekilde bir DOM öğesi döndürecek.
+// Kullanılan etiketler, öğelerin hiyerarşisi ve öznitelikleri sağlanan işaretlemeyle eşleşmelidir!
+// Öğelerin içindeki metin, "textContent" özelliği kullanılarak ayarlanacaktır ("innerText" DEĞİL).
+//
+// <div class="topics">
+//   <div class="tab">javascript</div>
+//   <div class="tab">bootstrap</div>
+//   <div class="tab">teknoloji</div>
+// </div>
+
+import { response } from "express";
+import { errors } from "msw/lib/types/context";
+
+//
 const Tablar = (konu) => {
-  // GÖREV 3
-  // ---------------------
-  // Tek argümanı bir dizi ("konu") olan bu fonksiyonu uygulayın.
-  // Örnek olarak, konu dizisi şu şekilde deklare edilmişse ['javascript', 'bootstrap', 'teknoloji']
-  // fonksiyon aşağıdaki şekilde bir DOM öğesi döndürecek.
-  // Kullanılan etiketler, öğelerin hiyerarşisi ve öznitelikleri sağlanan işaretlemeyle eşleşmelidir!
-  // Öğelerin içindeki metin, "textContent" özelliği kullanılarak ayarlanacaktır ("innerText" DEĞİL).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">teknoloji</div>
-  // </div>
-  //
-}
+  const div2 = document.createElement("div");
+  div2.classList.add("topics");
 
+  for (let lanet of konu) {
+    const div3 = document.createElement("div");
+
+    div3.textContent = lanet;
+    div2.appendChild(div3);
+  }
+
+  return div2;
+};
+// GÖREV 4
+// ---------------------
+// Tek argümanı olarak bir css seçici alan bu işlevi uygulayın.
+// Konuları bu uç noktadan almalıdır: `http://localhost:5001/api/konular` (console.log ile test edin!).
+// Yanıtın içindeki konu dizisini bulun ve Tablar bileşenini kullanarak tabları oluşturun.
+// Tabları, fonksiyona iletilen seçiciyle eşleşen DOM'daki öğeye ekleyin.
+//
 const tabEkleyici = (secici) => {
-  // GÖREV 4
-  // ---------------------
-  // Tek argümanı olarak bir css seçici alan bu işlevi uygulayın.
-  // Konuları bu uç noktadan almalıdır: `http://localhost:5001/api/konular` (console.log ile test edin!).
-  // Yanıtın içindeki konu dizisini bulun ve Tablar bileşenini kullanarak tabları oluşturun.
-  // Tabları, fonksiyona iletilen seçiciyle eşleşen DOM'daki öğeye ekleyin.
-  //
-}
+  const apiUrl = "http://localhost:5001/api/konular";
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const diziKonu = data.konu;
+      const element = Tablar(diziKonu);
+      const sonuc = document.querySelector(secici);
+      sonuc.appendChild(element);
+    })
+    .catch((error) => console.log("API hatası:", error));
+};
 
-export { Tablar, tabEkleyici }
+export { Tablar, tabEkleyici };
